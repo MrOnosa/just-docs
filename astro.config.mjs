1826089a0e30c9c999e025375219c166826024da
MrOnosa <supersnapple@gmail.com>
2026-07-17 20:12:30 -0500

    'extension' -> browser-extension'. Cleaned up unused imports. Added index and introduction sections where I feel like they make sense.

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,7 +45,7 @@ export default defineConfig({
                 {label: 'Commands', slug: 'commands'},
                 {label: 'Redeems', slug: 'redeems'},
                 {label: 'Usercard', slug: 'usercard'},
-                {label: 'Extension', slug: 'extension'},
+                {label: 'Extension', slug: 'browser-extension'},
                 {
                     label: 'API documentation',
                     link: 'https://api.bahms.org',
